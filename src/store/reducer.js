5939ca9914727a578ebd6735520fbb707cf97ce5
DariaDrozd <reddymargot@gmail.com>
2023-07-01 13:46:00 +0300

    fixed all bugs + styles

--- src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import { contactsReducer } from "./phonebook/contactsReducer";
-import { filterReducer } from "./phonebook/filterReducer";
-
-
-export const reducer = {
-    phonebok: contactsReducer,
-    filter: filterReducer
-}
